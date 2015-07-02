56538591c94d8711030b0ca083ee76c6994b7d08
aibin <aibin@home-pc>
2015-07-02 21:22:02 +0800

    tset

--- softrender/tool.h
+++ softrender/tool.h
@@ -383,6 +383,8 @@ inline float InvSqrt(float x)
 	i = 0x5f375a86- (i>>1);  // gives initial guess y0
 	x = *(float*)&i;         // convert bits back to float
 	x = x*(1.5f-xhalf*x*x);  // Newton step, repeating increases accuracy
+	//one more repeat
+	//x = x*(1.5f-xhalf*x*x);
 	return x;
 }
 inline float mod_pi(float x)
